6850f917f03a4b6cc9d6e3f81380e8f5b42e0c04
antonich k <antonich.nz@gmail.com>
2021-10-29 21:51:47 +1300

    in progress

---
+++ ValidateAnExpression/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ValidateAnExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
